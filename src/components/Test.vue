aa730f0baa1c86221b15c1b599ec499c6b7a29e3
Dorr-hlc <1726660621@qq.com>
2023-05-27 22:09:40 +0800

    feat: 新增配置

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,8 +1,5 @@
 <template>
-  <div>
-    <h1>svg测试</h1>
-
-  </div>
+  <div>111</div>
 </template>
 <script setup lang="ts"></script>
 <style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { createApp } from 'vue'
 
-import App from './App.vue'
+import App from '@/App.vue'
+// 引入element-plus插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置elementplus国际化
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// svg插件
+import 'virtual:svg-icons-register'
+const app = createApp(App)
+// 安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 
-createApp(App).mount('#app')
-
-const fun = () => {
-  console.log('我爱你')
-}
-
-fun()
+// 将应用挂载
+app.mount('#app')
