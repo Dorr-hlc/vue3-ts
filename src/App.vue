d8ca3555d78a60087ff63d41fea137301078bb99
Dorr-hlc <1726660621@qq.com>
2023-05-27 13:12:08 +0800

    提交未格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">我是APP根组件</div>
+</template>
+<script setup lang="ts">
+const str = '我爱你罪'
+</script>
+<style scoped lang="scss">
+.box {
+  width: 800px;
+  height: 400px;
+  background: red;
+
+  h1 {
+    color: #fff;
+  }
+}
+</style>
