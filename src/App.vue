e5bd93ea2ba91e16a958252277a582c6af1fb8a2
Dorr-hlc <1726660621@qq.com>
2023-05-28 21:54:22 +0800

    feat: svg全局组件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <h1>svg测试</h1>
-
+    <svg-icon
+      name="copyright"
+      color="red"
+      width="100px"
+      height="100px"
+    ></svg-icon>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SvgIcon from '@/components/SvgIcon/index.vue'
+</script>
 <style scoped lang="scss"></style>
---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <svg :style="{ width, height }">
+      <use :xlink:href="prefix + name" :fill="color"></use>
+    </svg>
+  </div>
+</template>
+<script setup lang="ts">
+// 接受父组件传递的参数
+defineProps({
+  prefix: {
+    type: String,
+    default: '#icon-',
+  },
+  // 提供使用的图标名字
+  name: {
+    type: String,
+  },
+  color: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: String,
+    default: '16px',
+  },
+  height: {
+    type: String,
+    default: '16px',
+  },
+})
+</script>
+<style scoped lang="scss"></style>
